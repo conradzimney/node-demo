24229a6b148785a75dc08ed6fb585a54b84993d4
Conrad Z <conradzimney@gmail.com>
2014-11-24 12:02:44 -0800

    after class changes

--- hello-server.js
+++ hello-server.js
@@ -6,3 +6,17 @@
 
 'use strict';
 
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+   res.send('Hello World from Zimney');
+});
+
+app.get('/foo', function(req, res) {
+   res.send('You requested foo');
+});
+
+app.listen(8080, function() {
+   console.log('Listening on http://localhost:8080');
+});
